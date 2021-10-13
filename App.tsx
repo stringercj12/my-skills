b02120de267bb2f5d25c1012404f84b66216ad8a
Jefferson Ferreira <jefferson14489@gmail.com>
2021-10-13 14:53:00 -0300

    Melhorando tela de splash

--- App.tsx
+++ App.tsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { StatusBar } from "react-native";
+import SplashScreen from 'react-native-splash-screen'
+
+
 import { Home } from "./src/pages/Home";
 
 export default function App() {
+
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <>
       <StatusBar
         barStyle="light-content"
-        color="#FFFFFF"
         translucent
       />
       <Home />
